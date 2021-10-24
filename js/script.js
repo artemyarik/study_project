0eeb9f74569b06d8ec9f6ab7c5566c74001706d7
Artem <artem.yasnov1@live.com>
2021-10-24 22:45:51 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("1");
+"use strict";
+
+
+const numberoffiels = prompt("Сколько?", '');
+
+const personalMovie = {
+    count: numberoffiels,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из просмотренных?', ''),
+        b = prompt('На сколько оцените?', ''),
+        c = prompt('Один из просмотренных?', ''),
+        d = prompt('На сколько оцените?', '');
+
+personalMovie.movies[a] = b;
+personalMovie.movies[c] = d;
+
+console.log(personalMovie);
